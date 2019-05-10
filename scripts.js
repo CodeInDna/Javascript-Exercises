aa24bc57fee1b09e9414625cf63c17499fe48c16
CodeInDna <ykarelia@gmail.com>
2019-05-10 10:14:02 +0530

    Starter Template

---
+++ scripts.js
@@ -0,0 +1,5 @@
+const videoEl = document.querySelector('.player');
+const canvasEl = document.querySelector('.photo');
+const ctx = canvas.getContext('2d');
+const stripEl = document.querySelector('.strip');
+const snapEl = document.querySelector('.snap');
